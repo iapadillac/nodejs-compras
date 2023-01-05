b217ba9485cd8e5d0c63df385357318d2abf8eb7
unknown <iapadillac@utn.edu.ec>
2023-01-04 22:49:47 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,16 @@
 const pgPromise = require ('pg-promise')
-import {DB_HOST,
-DB_NAME,
-DB_PASSWORD,
-DB_PORT,
-DB_USER} from './config.js'
+
 const config = {
 
-host : DB_HOST,
-port : DB_PORT,
-database: DB_NAME,
-user:DB_USER,
-password: DB_PASSWORD,
-/**ssl: {
+host : 'ec2-52-54-212-232.compute-1.amazonaws.com',
+port : '5432',
+database: 'd4e2f1o7be6ake',
+user:'fktfwqvtruhass',
+password: 'b66c50fcd86119639dd8fef2e27cb0eb03d4dfa66f7be6b0a85b43a1d6762432',
+ssl: {
     rejectUnauthorized: false, 
   },
-  **/
+  
 }
 
 const pgp = pgPromise({})

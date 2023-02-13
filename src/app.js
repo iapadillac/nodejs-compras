a8f3fbe2b20db6f7b7c796fe47768e7fd224edfa
unknown <iapadillac@utn.edu.ec>
2023-02-12 23:24:12 -0500

    make it better

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://appfrontendcompras.herokuapp.com/');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');

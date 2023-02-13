a2e5a172dfc55348b559a46053a754228ccda8d6
unknown <iapadillac@utn.edu.ec>
2023-02-13 01:46:42 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -9,33 +9,6 @@ const bodyParser = require("body-parser");
 const whitelist= ['https://appfrontendcompras.herokuapp.com']
 app.use(cors({origin:whitelist}))
 
-
-
-
-
-///origen header/////
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
-// other app.use() options ...
-app.use(expressCspHeader({ 
-  policies: { 
-      'default-src': [expressCspHeader.NONE], 
-      'img-src': [expressCspHeader.SELF], 
-  } 
-})); 
-
-
-
-
-
-
-
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
